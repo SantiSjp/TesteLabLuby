9d092dd092f00cd5961c8c526752252f81f8321a
santisjp <santisjp@live.com>
2021-01-23 23:40:29 -0400

    Criando projeto de desafio de POO

---
+++ 3DesafioPOO/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3DesafioPOO
+{
+    public class Produto
+    {
+        public int Nome { get; set; }
+        public double Valor { get; set; }
+    }
+}
